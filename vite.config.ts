d602219fe2d6090a6504860b255a1036cdb57913
luo <2731800406@qq.com>
2024-08-20 09:48:32 +0800

    ``` feat(devDependencies): 添加volar、unplugin-vue-macros和vite-plugin-inspect
    添加了@vue-macros/volar、unplugin-vue-macros和vite-plugin-inspect到devDependencies，以提升项目开发体验。这些依赖分别用于提供Vue语法支持、宏插件机制和Vite插件检查工具。
    ```

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
-import vue from '@vitejs/plugin-vue';
+import Vue from '@vitejs/plugin-vue';
+import VueMacros from 'unplugin-vue-macros/vite';
 import { defineConfig } from 'vite';
+import Inspect from 'vite-plugin-inspect';
 import qiankun from 'vite-plugin-qiankun';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "./",
     plugins: [
-        vue(),
+        VueMacros({
+            plugins: {
+                vue: Vue(),
+            },
+        }),
+
+        Inspect(),
+
         qiankun("qiankun-vue-system", {
             useDevMode: true
-        })
+        }),
     ],
     server: {
         port: 13100,
